d370128da1a5f9b7a26c3d3e8cc8a5395a3c0be7
Parsa H. Rafsanjani <parsahosseini2001@gmail.com>
2025-10-29 11:27:41 +0330

    Add functions for the first theorem

---
+++ R/simple_beta.r
@@ -0,0 +1,9 @@
+simple_simple_beta() <- function(theta0, theta1, r, n, m, alpha) {
+    if (theta1 ^ r > theta0 ^ r) {
+        c <- qgamma(alpha, shape = n * m / r, scale = 1 / theta0 ^ r)
+        pgamma(c, shape = n * m / r, scale = 1 / theta1 ^ r)
+    } else {
+        c <- qgamma(1 - alpha, shape = n * m / r, scale = 1 / theta0 ^ r)
+        1 - pgamma(c, shape = n * m / r, scale = 1 / theta1 ^ r)
+    }
+}
